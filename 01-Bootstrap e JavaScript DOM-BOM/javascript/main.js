8ab9aa4a636a158b8755e140a5bc33c358dffccc
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-09 16:27:12 -0300

    UPDATE: using DOM methods to change values

--- 01-Bootstrap e JavaScript DOM-BOM/javascript/main.js
+++ 01-Bootstrap e JavaScript DOM-BOM/javascript/main.js
@@ -1,1 +1,15 @@
-document.getElementById('demo').innerHTML ='Hello world';
+const element = document.getElementById('intro');
+document.getElementById('demo').innerHTML = `TEXTO: ${element.innerHTML}`;
+//-------------------------------------
+const element2 = document.getElementsByTagName('p');
+document.getElementById('demo2').innerHTML = `TEXTO: ${element2[3].innerHTML}`;
+//-------------------------------------
+
+const x = document.getElementById('main');
+const y = x.getElementsByTagName('p');
+
+console.log('chega aq')
+document.getElementById('demo3').innerHTML = `TEXTOS: ${y[1].innerHTML}.` 
+
+
+//-------------------------------------
