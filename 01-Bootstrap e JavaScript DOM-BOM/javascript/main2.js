2210c4353932cb12520b248a7c95573b2cdef896
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-09 16:47:38 -0300

    UPDATE: methods created to change content

---
+++ 01-Bootstrap e JavaScript DOM-BOM/javascript/main2.js
@@ -0,0 +1,9 @@
+document.getElementById('p1').innerHTML = 'new text'
+
+//----------------------------------
+const element = document.getElementById('h1');
+element.innerHTML = 'New Heading';
+
+//----------------------------------
+
+document.getElementById('img').src="img/ti.02.jpg";
