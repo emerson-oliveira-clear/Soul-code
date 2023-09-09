815914245d6cc6f4d20f1177d779c618757d7939
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-09 18:36:56 -0300

    UPDATE: methods created to capture events

---
+++ 01-Bootstrap e JavaScript DOM-BOM/javascript/main6.js
@@ -0,0 +1,20 @@
+function changeText(id){
+
+    id.innerHTML ='MUDOU';
+
+}
+function displayDate(){
+
+    document.getElementById('demo').innerHTML = Date()
+
+}
+
+function mOver(obj){
+    obj.innerHTML = 'Obrigado!'
+
+}
+
+function mOut(obj){
+    obj.innerHTML = 'Mouse Over me!'
+    
+}
