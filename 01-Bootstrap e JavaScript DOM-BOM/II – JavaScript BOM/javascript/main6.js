c6fcd64885c158afd15eb597a52e65261f616da7
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-10 10:09:10 -0300

    UPDATE: methods for setting cookies

---
+++ 01-Bootstrap e JavaScript DOM-BOM/II – JavaScript BOM/javascript/main6.js
@@ -0,0 +1,9 @@
+username = 'jose';
+
+document.cookie = 'username=jose';
+
+document.cookie = 'username=jose; expires=Thu, 18 Dec 2022 12:00 UTC';
+
+let x = document.cookie;
+
+document.cookie = 'username=jose; expires=Thu, 01 Jan 1980 00:00 UTC; path=/';
