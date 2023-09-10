bcec0fb0c76e35333da4329cc144f7fa759db0ba
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-10 10:02:10 -0300

    NOTE:  old class with obsolete methods

---
+++ 01-Bootstrap e JavaScript DOM-BOM/II – JavaScript BOM/javascript/main5.js
@@ -0,0 +1,22 @@
+
+//verificar o idioma da pagina
+document.getElementById('p1').innerHTML = navigator.language;
+
+//habilitar cookies do navegador
+document.getElementById('p2').innerHTML = navigator.cookieEnabled;
+
+//não usa mais :<
+document.getElementById('p3').innerHTML = navigator.appName;
+
+//não usa mais :<
+document.getElementById('p4').innerHTML = navigator.appCodeName;
+
+//não usa mais :<
+document.getElementById('p5').innerHTML = navigator.product;
+
+//não usa mais :<
+document.getElementById('p6').innerHTML = navigator.appVersion;
+
+
+document.getElementById('p7').innerHTML = navigator.userAgent;
+
