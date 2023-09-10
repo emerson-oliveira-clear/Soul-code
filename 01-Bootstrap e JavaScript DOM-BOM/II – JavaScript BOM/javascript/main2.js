18f35cb190eed699de08c76da1b1492129bc117b
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-10 09:07:43 -0300

    UPDATE: methods to capture screen measurements

---
+++ 01-Bootstrap e JavaScript DOM-BOM/II – JavaScript BOM/javascript/main2.js
@@ -0,0 +1,17 @@
+
+// verificar o tamanha da tela
+document.getElementById('p1').innerHTML = screen.width;
+document.getElementById('p2').innerHTML = screen.height;
+
+
+// verificar o tamanha da tela disponivel
+document.getElementById('p3').innerHTML = screen.availWidth;
+document.getElementById('p4').innerHTML = screen.availHeight;
+
+
+// verificar a profundidade de cores
+document.getElementById('p5').innerHTML = screen.colorDepth;
+
+
+// verificar a profundidade de pixels
+document.getElementById('p6').innerHTML = screen.pixelDepth;
