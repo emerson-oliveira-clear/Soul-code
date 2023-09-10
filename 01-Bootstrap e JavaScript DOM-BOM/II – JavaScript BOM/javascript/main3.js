31b2fc4437072dde0b2ee6d62258f0d31af9b522
emerson-oliveira-clear <emerson.oliveira.clear@gmail.com>
2023-09-10 09:29:28 -0300

    UPDATE: Methods for Capturing Pathfinding

---
+++ 01-Bootstrap e JavaScript DOM-BOM/II – JavaScript BOM/javascript/main3.js
@@ -0,0 +1,20 @@
+
+
+document.getElementById('p1').innerHTML =window.location.href;
+
+
+
+document.getElementById('p2').innerHTML =window.location.protocol;
+
+
+
+document.getElementById('p3').innerHTML =window.location.pathname;
+
+
+
+document.getElementById('p4').innerHTML =window.location.hostname;
+
+
+
+document.getElementById('p5').innerHTML =window.location.port;
+
